897add31acfa1f0b986d2875ff8e6fce5377b0cb
WON <aekik1118@naver.com>
2017-07-12 16:01:00 +0900

    경기장 이미지 가져오기

--- app/src/main/java/org/techtown/dolmonoproto/jongmok.java
+++ app/src/main/java/org/techtown/dolmonoproto/jongmok.java
@@ -1,24 +1,90 @@
 package org.techtown.dolmonoproto;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.os.AsyncTask;
+
 
 /**
  * Created by aekik on 2017-07-07.
  */
 
+
 public class jongmok extends Fragment{
     View v;
+    public static final String baseImageURL = "http://2017sports.chungbuk.go.kr/DATA/stadium/20170203115418704TcnTJX.jpg";
+    ImageView iv;
+    Bitmap bm;
+    private ProgressDialog progressDialog;
+
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.jongmok_layout, container, false);
+
+        iv = (ImageView) v.findViewById(R.id.imageView24);
+
+        Thread mThread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    URL url = new URL(baseImageURL);
+
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream();
+                    bm = BitmapFactory.decodeStream(is);
+
+
+
+
+                }catch(IOException ex)
+                {
+
+                }
+
+
+            }
+
+        };
+
+        mThread.start();
+
+        try{
+            mThread.join();
+            iv.setImageBitmap(bm);
+
+        }catch (InterruptedException e){
+
+        }
 
 
 
