ccd9a2e9b8c73f6c10d452357e4e001ac28d4fca
aekik1118 <aekik1118@naver.com>
2017-07-21 20:07:24 +0900

    초기 화면 추가 그림 미정

---
+++ app/src/main/java/org/techtown/dolmonoproto/opening.java
@@ -0,0 +1,38 @@
+package org.techtown.dolmonoproto;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by aekik on 2017-07-21.
+ */
+public class opening extends Activity {
+
+    Handler handler = new Handler();
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+
+        }
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.openning_layout);
+
+        handler.postDelayed(r,2000);
+
+
+    }
+
+
+}
