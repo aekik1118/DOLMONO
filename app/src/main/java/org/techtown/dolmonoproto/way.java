e861174e016588984d51798fc85da249dfe956a4
k0ldzer0 <k0ldzer0@naver.com>
2017-07-22 22:18:03 +0900

    개요 - 찾아오시는길 구글맵 적용

--- app/src/main/java/org/techtown/dolmonoproto/way.java
+++ app/src/main/java/org/techtown/dolmonoproto/way.java
@@ -7,20 +7,48 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by aekik on 2017-06-26.
  */
 
 
 
-public class way extends Fragment{
+public class way extends Fragment
+        implements OnMapReadyCallback
+{
+    private GoogleMap googleMap = null;
+    private MapView mapView = null;
+
+
     View v;
     MainActivity activity;
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.way_layout, container, false);
+
+        mapView = (MapView) v.findViewById(R.id.map);
+        mapView.getMapAsync(this);
+
+
 
         activity = (MainActivity) getActivity();
 
@@ -30,6 +58,78 @@ public class way extends Fragment{
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//액티비티가 처음 생성될 때 실행되는 함수
+
+        if(mapView != null)
+        {
+            mapView.onCreate(savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng chungju = new LatLng(36.960061, 127.916636);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        markerOptions.position(chungju);
+
+        markerOptions.title("충주 종합운동장");
+
+        markerOptions.snippet("주경기장");
+
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(chungju));
+
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+    }
 
 
 }
